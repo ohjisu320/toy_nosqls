e1a81d3e0dfbba5f244a803314da5dc7de2002bd
scvgong <gongmyeongyun@gmail.com>
2023-12-20 11:18:24 +0900

    aa

--- test_result.py
+++ test_result.py
@@ -12,19 +12,19 @@ collection = connect('result') # result collection 연결
 
 
 def cal_score():
-    user_name = ["",""]
+    user_name = ["",""] # user 이름 받을 초기화 변수
     user_answer = [[0,0,0,0,0],[0,0,0,0,0]]# one,two 사용자 답항 변수, 추후 초기화
-    list_answer = [3,2,4,4,5]
+    list_answer = [3,2,4,4,5] # 정답에 대한 인덱스
 
     list_score = [20,30,15,20,15] # 답항별 점수
 
     #정답 3,2,4,4,5
 
-    user_score = [0,0]
+    user_score = [0,0] # 점수 초기 변수
 
     # 사용자 입력 및 점수 계산
     for x in range(len(user_name)):
-        input_user = input("사용자 이름 : ")
+        input_user = input("사용자 이름 : ") # user 입력
         user_name[x] = input_user
 
         for i in range(len(user_answer[x])): #문제에 대한 답변 입력
