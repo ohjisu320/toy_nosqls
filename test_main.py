6323459c382efce3b422dfd27402617c680e8972
ohjisu320 <ohjisu320@gmail.com>
2023-12-21 12:34:24 +0900

    modify

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 # import한 function 작동하는지 확인하기
-# import test_contents 
-# test_contents.make_test()
+import test_contents 
+test_contents.make_test()
 import test_ing_merged
 test_ing_merged.user_interface()
 import test_result
